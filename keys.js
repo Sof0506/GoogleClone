1758232fb0ffe155f630122c7fcc31c9f195af19
SofBen <106510033+Sof0506@users.noreply.github.com>
2022-08-24 00:38:59 +0100

    Update keys.js

--- keys.js
+++ keys.js
@@ -1,4 +1,4 @@
 //API KEY
-export const API_KEY=' AIzaSyCVKlY8uzGpeZG9x7FzX_G8W4HgmE2qDBM '
+export const API_KEY=' API_KEY '
 //Context KEY
-export const CONTEXT_KEY='9779c2ed5d7ed45a3'
+export const CONTEXT_KEY='CX key'
